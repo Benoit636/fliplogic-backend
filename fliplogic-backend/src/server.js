6720b2344cf553c27f671fcdc24a0a7be42d580b
Benoit636 <benoitboudreau@vwmoncton.com>
2026-04-23 20:08:34 -0300

    Update server.js

--- fliplogic-backend/src/server.js
+++ fliplogic-backend/src/server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
@@ -18,4 +18,4 @@ app.listen(PORT, () => {
 console.log(`FlipLogic API running on port ${PORT}`);
 });
 
-export default app;
+module.exports = app;
